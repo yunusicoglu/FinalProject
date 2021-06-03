fa5f05629cee6c7f49b63cfa35b44652e69f2307
Yunus İçoğlu <yunusicoglu1256@outlook.com>
2021-06-03 07:54:15 +0300

    03.06.2021

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,5 +9,7 @@ namespace Business.Constants
     {
         public static string ProductAdded = "Ürün eklendi";
         public static string ProductNameInvalid = "Ürün ismi gecersiz";
+        public static string MaintenanceTime = "Sistem bakimda";
+        public static string ProductsListed = "Urunler listelendi";
     }
 }
